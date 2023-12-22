7271d291fee2e4e232d8f8d7e59a35e5446a60b4
ZakiHani <zakaria20haniri@gmail.com>
2023-12-21 23:30:40 -0500

    setup cluster

--- Setup/setup_cluster.py
+++ Setup/setup_cluster.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     # Get credentials from the config file :
     path = os.path.dirname(os.getcwd())
     config_object = configparser.ConfigParser()
-    with open(path+"/credentials.ini","r") as file_object:
+    with open(path+"/my_credentials.ini","r") as file_object:
         #Loading of the aws tokens
         config_object.read_file(file_object)
         key_id = config_object.get("resource","aws_access_key_id")
@@ -20,20 +20,20 @@ if __name__ == '__main__':
         ami_id = config_object.get("ami","ami_id")
 
 
-    print('============================>SETUP Begins')
+    print('============================>The begining of setup')
 
-    #--------------------------------------Creating ec2 resource and client ----------------------------------------
+    #--------------------------------------ec2 resource and client creations----------------------------------------
     
     #Create ec2 resource with our credentials:
     ec2_serviceresource = resource_ec2(key_id, access_key, session_token)
-    print("============> ec2 resource creation has been made succesfuly!!!!<=================")
+    print("============> Successful creation of ec2 resource <=================")
     #Create ec2 client with our credentials:
     ec2_serviceclient = client_ec2(key_id, access_key, session_token)
-    print("============> ec2 client creation has been made succesfuly!!!!<=================")
+    print("============> Successful creation of ec2 client <=================")
 
-    #--------------------------------------Creating a keypair, or check if it already exists-----------------------------------
+    #--------------------------------------keypair creation or check of prior existance-----------------------------------
     
-    key_pair_name = create_keypair('lab1_keypair', ec2_serviceclient)
+    key_pair_name = create_keypair('final_keypair', ec2_serviceclient)
 
     #---------------------------------------------------Get default VPC ID-----------------------------------------------------
     #Get default vpc description : 
@@ -49,10 +49,10 @@ if __name__ == '__main__':
     vpc_id = default_vpc_desc[0].get('VpcId')
 
 
-    #--------------------------------------Try create a security group with all traffic inbouded--------------------------------
+    #--------------------------------------Try create a security group--------------------------------
   
     try:
-        security_group_id = create_security_group("All traffic sec_group","lab1_security_group",vpc_id,ec2_serviceresource)  
+        security_group_id = create_security_group("sec_group","final_security_group",vpc_id,ec2_serviceresource)  
     
     except :
         #Get the standard security group from the default VPC :
@@ -67,7 +67,7 @@ if __name__ == '__main__':
         {
                 'Name': 'group-name',
                 'Values': [
-                    "lab1_security_group",
+                    "final_security_group",
                 ]
             },
 
@@ -76,42 +76,31 @@ if __name__ == '__main__':
         security_group_id = (sg_dict.get("SecurityGroups")[0]).get("GroupId")
     
 
-    #--------------------------------------Pass flask deployment script into the user_data parameter ------------------------------
-    with open('flask_orchestrator.sh', 'r') as f :
-        flask_script_orchestrator = f.read()
+    #--------------------------------------Pass sysbench and mysqlinstallation script for master and slaves script into the user_data parameter ------------------------------
+    with open('sysbench_mysql_master_slaves.sh', 'r') as f :
+        sysbench_mysql_master_slaves = f.read()
 
-    ud_orchestrator = str(flask_script_orchestrator)
+    sysbench_mysql = str(sysbench_mysql_master_slaves)
 
-    with open('flask_workers.sh', 'r') as f :
-        flask_script_worker = f.read()
+    #--------------------------------------Create Instances of master and slaves with installing sysbench and mysql on them ------------------------------------------------------------
 
-    ud_workers = str(flask_script_worker)
-    ud_orchestrator=''
-
-    #--------------------------------------Create Instances of orchestrator and workers ------------------------------------------------------------
-
-    # Create 4 intances with m4.large as workers:
-    Availabilityzons_Cluster1=['us-east-1a','us-east-1b','us-east-1a','us-east-1b','us-east-1a']
-    instance_type = "m4.large"
-    print("\n Creating instances : the workers ")
-
-    # Creation of the 4 workers
-    workers_m4= create_instance_ec2(4,ami_id, instance_type,key_pair_name,ec2_serviceresource,security_group_id,Availabilityzons_Cluster1,"worker",ud_workers)
-
-    #Update test.json with the new workers ip
-    update_test_json(workers_m4)
+    # Create master t2.micro instance:
+    instance_type = "t2.micro"
     
-    time.sleep(330)
-    print('\n Waiting for deployement of flask application on workers containers ....\n')
-    
-    #Modify the flask_orchestrator.sh file with the new containers ip
-    update_orchestrator_sh(ud_orchestrator)
+    print("\n Creating instance : master ")
+    # Creation of the master
+    master_t2= create_instance_ec2(1,ami_id, instance_type,key_pair_name,ec2_serviceresource,security_group_id,['us-east-1a'],"master",sysbench_mysql)
+    print("\n master created successfully with installation of sysbench and mysql")
 
-    print("\n Creating instances : Orchestrator ")
+    print("\n Creating instances : slaves ")
+    # Creation of the slaves
+    slaves_t2= create_instance_ec2(3,ami_id, instance_type,key_pair_name,ec2_serviceresource,security_group_id,['us-east-1a','us-east-1a','us-east-1a'],"slave",sysbench_mysql)
+    print("\n slaves created successfully with installation of sysbench and mysql")
 
-    # Creation of the orchestrator
-    orchestrator_m4=create_instance_ec2(1,ami_id, instance_type,key_pair_name,ec2_serviceresource,security_group_id,Availabilityzons_Cluster1,"orchestrator",ud_orchestrator)
 
-    print("\n Orchestrator and the 4 workers created successfully")
-    
+    #--------------------------------------Config mysql on master and slaves ------------------------------------------------------------
+
+    #--------------------------------------Installing sakila on master and slaves ------------------------------------------------------------
+
+
     print('============================>SETUP ends')
