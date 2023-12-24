5560a01073f8142c00c756de7d8649671b0bfd34
ZakiHani <zakaria20haniri@gmail.com>
2023-12-24 02:13:15 -0500

    master mysql config

--- Setup/sysbench_mysql_master_slaves.sh
+++ Setup/sysbench_mysql_master_slaves.sh
@@ -6,28 +6,23 @@ sudo apt-get install sysbench -y
 #Initiation the installation of mysql
 sudo service mysqld stop
 sudo apt remove mysql-server mysql mysql-devel
-
-home_cluster="/opt/mysqlcluster/home"
-
-sudo mkdir -p "$home_cluster"
-cd "$home_cluster"
+sudo mkdir -p /opt/mysqlcluster/home
+cd /opt/mysqlcluster/home
 sudo wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 sudo tar xvf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 sudo ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysqlc
 
-echo 'export MYSQLC_HOME='"$home_cluster"'/mysqlc' | sudo tee -a /etc/profile.d/mysqlc.sh
+echo 'export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc' | sudo tee -a /etc/profile.d/mysqlc.sh
 echo 'export PATH=$MYSQLC_HOME/bin:$PATH' | sudo tee -a /etc/profile.d/mysqlc.sh
-
 source /etc/profile.d/mysqlc.sh
 sudo apt-get update
 sudo apt-get -y install libncurses5
 
-deploy_cluster="/opt/mysqlcluster/deploy"
-conf_cluster="/opt/mysqlcluster/deploy/conf"
-
-cd "$deploy_cluster"
-sudo mkdir conf mysqld_data ndb_data 
-cd "$conf_cluster"
+cd "/opt/mysqlcluster/deploy"
+sudo mkdir conf 
+sudo mkdir mysqld_data 
+sudo mkdir ndb_data 
+cd conf
 
 echo -e "[mysqld]" | sudo tee -a my.cnf
 echo -e "ndbcluster" | sudo tee -a my.cnf
@@ -36,7 +31,7 @@ echo -e "basedir=/opt/mysqlcluster/home/mysqlc" | sudo tee -a my.cnf
 echo -e "port=3306" | sudo tee -a my.cnf
 
 echo -e "[ndb_mgmd]" | sudo tee -a config.ini
-echo -e "hostname=ip-172-31-21-57.ec2.internal" | sudo tee -a config.ini
+echo -e "hostname=ip-172-31-20-30.ec2.internal" | sudo tee -a config.ini
 echo -e "datadir=/opt/mysqlcluster/deploy/ndb_data" | sudo tee -a config.ini
 echo -e "nodeid=1" | sudo tee -a config.ini
 
@@ -45,17 +40,17 @@ echo -e "noofreplicas=3" | sudo tee -a config.ini
 echo -e "datadir=/opt/mysqlcluster/deploy/ndb_data" | sudo tee -a config.ini
 
 echo -e "[ndbd]" | sudo tee -a config.ini
-echo -e "hostname=ip-172-31-25-67.ec2.internal" | sudo tee -a config.ini
+echo -e "hostname=ip-172-31-26-136.ec2.internal" | sudo tee -a config.ini
 echo -e "datadir=/opt/mysqlcluster/deploy/ndb_data" | sudo tee -a config.ini
 echo -e "nodeid=2" | sudo tee -a config.ini
 
 echo -e "[ndbd]" | sudo tee -a config.ini
-echo -e "hostname=ip-172-31-24-233.ec2.internal" | sudo tee -a config.ini
+echo -e "hostname=ip-172-31-20-139.ec2.internal" | sudo tee -a config.ini
 echo -e "datadir=/opt/mysqlcluster/deploy/ndb_data" | sudo tee -a config.ini
 echo -e "nodeid=3" | sudo tee -a config.ini
 
 echo -e "[ndbd]" | sudo tee -a config.ini
-echo -e "hostname=ip-172-31-19-210.ec2.internal" | sudo tee -a config.ini
+echo -e "hostname=ip-172-31-18-70.ec2.internal" | sudo tee -a config.ini
 echo -e "datadir=/opt/mysqlcluster/deploy/ndb_data" | sudo tee -a config.ini
 echo -e "nodeid=4" | sudo tee -a config.ini
 
