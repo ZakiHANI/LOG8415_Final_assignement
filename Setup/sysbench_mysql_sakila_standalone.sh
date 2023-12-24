214814beaad922069a932eb51c2816448aac13b0
ZakiHani <zakaria20haniri@gmail.com>
2023-12-24 17:23:36 -0500

    test standalone

--- Setup/sysbench_mysql_sakila_standalone.sh
+++ Setup/sysbench_mysql_sakila_standalone.sh
@@ -1,18 +1,15 @@
 
-sudo apt-get update
-
-#Installation of sysbench
-sudo apt-get install sysbench -y
+sudo apt-get update -y
 
 #Installation of mysql
 sudo apt-get install mysql-server -y
+sudo apt-get install sysbench -y
 
-#Installation of sakila
-sakila_dir='home/sak'
-sudo mkdir "$sakila_dir"
-cd "$sakila_dir"
 sudo wget https://downloads.mysql.com/docs/sakila-db.tar.gz
 sudo  tar xvf sakila-db.tar.gz
 
-sudo mysql SOURCE /home/sak/sakila-db/sakila-schema.sql;
-sudo mysql SOURCE /home/sak/sakila-db/sakila-data.sql;
+mysql -u root -e "SOURCE /sakila-db/sakila-schema.sql;"
+mysql -u root -e "SOURCE /sakila-db/sakila-data.sql;"
+
+mysql -e "CREATE USER 'me'@'localhost' IDENTIFIED BY 'ZAKARIA';"
+mysql -e "GRANT ALL PRIVILEGES on sakila.* TO 'me'@'localhost';"
