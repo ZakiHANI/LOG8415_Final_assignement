ff7e8169ed927bc97da0ff731c510d1ede83b29c
ZakiHani <zakaria20haniri@gmail.com>
2023-12-25 21:58:27 -0500

    add proxy function of customize mode

--- Proxy/functions_proxy.py
+++ Proxy/functions_proxy.py
@@ -1,7 +1,8 @@
 import boto3
 import paramiko
 import random
-
+import time
+from ping3 import ping
 
 #Create the function that implement the proxy mode <Direct hit>
 #The idea is that we send a write request to the proxy and it will forword it to the master
@@ -18,7 +19,7 @@ def proxy_direct_hit(publicIpAddress_master,request):
     in1,out1,err1=ssh_master.exec_command("sudo echo '" + str(request) + "' >> requests.sql" )
     in2,out2,err2=ssh_master.exec_command("sudo /opt/mysqlcluster/home/mysqlc/bin/mysql -h 127.0.0.1 -u root < requests.sql")
     #Print the Output
-    print ('The output is:',out2)
+    print ('The output is:',out2.read())
     #Remove requests.sql
     in3,out3,err3=ssh_master.exec_command("sudo rm requests.sql")
 
@@ -26,12 +27,14 @@ def proxy_direct_hit(publicIpAddress_master,request):
 #Create the function that implement the proxy mode <Random>
 #The idea is that we send a read request to the proxy and it will forword it to a random slave
 
-def proxy_random(publicIpAddress_slaves_list,request,publicIpAddress_master):
-    'The function takes as arguments the list of publicIpAddress_slaves, the sent request and the publicIpAddress of the master'
+def proxy_random(dict_slaves_publicIpAddress,request,publicIpAddress_master):
+    'The function takes as arguments a dictianry (keys=slaves names and values=slaves publicIpAddresses),' 
+    'the sent request and the publicIpAddress of the master'
 
-    #Choice of a randme publicIpaddresse from publicIpaddresses of slaves
-    PublicIpAddress_chosen=random.choice(publicIpAddress_slaves_list)
-    print('The randomly chosen slave has the IP Address :',PublicIpAddress_chosen)
+    #Choice of a randome slave from list of slaves (dictionary keys)
+    chosen_slave=random.choice(dict_slaves_publicIpAddress.keys())
+    print('The randomly chosen slave is :',chosen_slave)
+    PublicIpAddress_chosen=dict_slaves_publicIpAddress[chosen_slave]
     #Create a SSH connection to the chosen slave using its chosen PublicIpAddress
     ssh_slave = paramiko.SSHClient()
     ssh_slave.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -39,9 +42,33 @@ def proxy_random(publicIpAddress_slaves_list,request,publicIpAddress_master):
     ssh_slave.connect(hostname=PublicIpAddress_chosen,username='ubuntu', pkey=key_private_slave)
     #Send the request to the chosen slave
     in1,out1,err1=ssh_slave.exec_command("sudo echo '" + str(request) + "' >> requests.sql" )
-    in2,out2,err2=ssh_slave.exec_command("sudo /opt/mysqlcluster/home/mysqlc/bin/mysql -h "+str(publicIpAddress_master).replace('.','-')+".compute-1.amazonaws.com -u ZAKARIA < requests.sql")
+    in2,out2,err2=ssh_slave.exec_command("sudo /opt/mysqlcluster/home/mysqlc/bin/mysql -h ec2-"+str(publicIpAddress_master).replace('.','-')+".compute-1.amazonaws.com -u ZAKARIA < requests.sql")
     #Print the Output
-    print ('The output is:',out2)
+    print ('The output is:',out2.read())
     #Remove requests.sql
     in3,out3,err3=ssh_slave.exec_command("sudo rm requests.sql")
+
+#Create the function that implement the proxy mode <Customize>
+#The idea is that we send a read request to the proxy and it will ping all the nodes and measure the ping time of each one ,
+#and then and send the request to the node with mimimum response time.
+def proxy_customize(dict_nodes_publicIpAddress,request,publicIpAddress_master):
+    'The function takes as arguments a dictianry (keys=nodes names and values=nodes publicIpAddresses) ,' 
+    'the sent request and the publicIpAddress of the master'
+
+    ping_times={node: ping('ec2-'+str(dict_nodes_publicIpAddress[node]).replace('.','-')+".compute-1.amazonaws.com") for node in dict_nodes_publicIpAddress.keys()}
+    Best_node=min(ping_times, key=ping_times.get)
+    print('The server with minimum ping is :',Best_node)
+    #Create a SSH connection to the selected node using its PublicIpAddress
+    ssh_selected_node = paramiko.SSHClient()
+    ssh_selected_node.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    key_private_selected_node = paramiko.RSAKey.from_private_key_file('final_keypair.pem')
+    ssh_selected_node.connect(hostname=dict_nodes_publicIpAddress[Best_node],username='ubuntu', pkey=key_private_selected_node)
+    #Send the request to the selected node 
+    in1,out1,err1=ssh_selected_node.exec_command("sudo echo '" + str(request) + "' >> requests.sql" )
+    in2,out2,err2=ssh_selected_node.exec_command("sudo /opt/mysqlcluster/home/mysqlc/bin/mysql -h ec2-"+str(publicIpAddress_master).replace('.','-')+".compute-1.amazonaws.com -u ZAKARIA < requests.sql")
+    #Print the Output
+    print ('The output is:',out2.read())
+    #Remove requests.sql
+    in3,out3,err3=ssh_selected_node.exec_command("sudo rm requests.sql")
+
 
