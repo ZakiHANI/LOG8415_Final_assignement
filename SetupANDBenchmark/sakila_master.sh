d5249d93ef6d1da0bba52292ac7478ab26a8c62d
ZakiHani <zakaria20haniri@gmail.com>
2023-12-28 00:08:40 -0500

    modify sakila _master.sh

--- SetupANDBenchmark/sakila_master.sh
+++ SetupANDBenchmark/sakila_master.sh
@@ -1,7 +1,4 @@
 #Installation of sakila
-# sakila_dir='home/sak'
-# sudo mkdir "$sakila_dir"
-# cd "$sakila_dir"
 
 cd /
 sudo wget https://downloads.mysql.com/docs/sakila-db.tar.gz
@@ -11,9 +8,6 @@ cd sakila-db
 
 mysql -u root -e "SOURCE sakila-schema.sql;"
 mysql -u root -e "SOURCE sakila-data.sql;"
-
-mysql -u root -e "USE sakila; SHOW FULL TABLES;"
-mysql -u root -e "USE sakila; SELECT COUNT(*) FROM film;"
 
 mysql -u root -e "GRANT ALL PRIVILEGES ON sakila.* TO 'root'@'%' IDENTIFIED BY '' WITH GRANT OPTION;"
 mysql -u root -e "FLUSH PRIVILEGES"
